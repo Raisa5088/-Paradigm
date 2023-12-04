6e6cbff6275005c45b8604586258e02a9305ac60
Elizaveta9988 <109989600+Elizaveta9988@users.noreply.github.com>
2023-12-04 15:44:15 +0300

    first commit

---
+++ Cat.java
@@ -0,0 +1,19 @@
+public class Cat extends Animal {
+    String color;
+
+    public Cat(String name, int age, String color) {
+        super(name, age);
+        this.color = color;
+    }
+
+    public void makeSound() {
+        System.out.println("Мяу-мяу!");
+
+    }
+
+    public void purr(){
+        System.out.println("Муррр");
+    }
+
+}
+
